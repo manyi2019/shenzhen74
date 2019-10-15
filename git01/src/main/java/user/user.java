aa482c63ef168d995cf31228800da0e887e7867d
manYi <2384739822@qq.com>
2019-10-15 10:16:01 +0800

    修改二次

--- git01/src/main/java/user/user.java
+++ git01/src/main/java/user/user.java
@@ -4,5 +4,6 @@ public class user {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("曹尼玛");
+        System.out.println("我是一只小小鸟");
     }
 }
